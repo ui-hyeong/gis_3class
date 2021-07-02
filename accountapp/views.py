957115bc34eaf1fc9ea2d1b14346a5c46813dc82
jeonguihyeong <jwh4861230@likelion.org>
2021-07-02 10:12:54 +0900

    7/2 3BAN CLASS COMMIT 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return HttpResponse('hello world') #alt enter를 통해 import를 해줄수 있음
+    return render(request, 'base.html')
