14bb340ad944a84a5b91bb8639565994bde9658c
jeonguihyeong <jwh4861230@likelion.org>
2021-07-09 09:34:07 +0900

    7 / 9 gis_p commit 1

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# 엑셀 시트가 될꺼임
+
+class newModel(models.Model):  # 장고에서 제공해주는 기본 모델 ( models.Model)
+    text = models.CharField(max_length=255, null=False)  # 문자열을 입력받는 것을 만든다. max_length는 최대 문자열 길이를 설정
