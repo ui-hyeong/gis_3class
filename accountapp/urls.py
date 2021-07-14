f12125534a2106efb7658cc1c48f39e9122995b6
jeonguihyeong <jwh4861230@likelion.org>
2021-07-14 15:58:10 +0900

    7 / 14 gis_p commit 3

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from accountapp.views import hello_world, AccountCreateView
@@ -6,6 +7,10 @@ app_name = 'accountapp'
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+
+    path('login/', LoginView.as_view(templates_name='accountapp/login.html'), name='login'),  # loginview는 장고에서 제공해주는 클래스
+
+    path('logout/', LogoutView.as_view(), name='logout'),
 
     path('create/', AccountCreateView.as_view(), name='create') # as_view를 선언하여 클래스를 사용할 수있게 한다.
 
