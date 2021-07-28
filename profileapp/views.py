a008e387df2732ea042cf73cb6d018d84d5263e3
jeonguihyeong <jwh4861230@likelion.org>
2021-07-28 15:23:43 +0900

    7 / 28 gis_p commit 2

--- profileapp/views.py
+++ profileapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
@@ -19,3 +19,11 @@ class ProfileCreateView(CreateView):
     def form_valid(self, form):    #요청한 유저를 식별하고 그유저에게 해당시킴
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    context_object_name = 'target_profile'
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/update.html'
