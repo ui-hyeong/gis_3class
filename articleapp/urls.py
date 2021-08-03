6528868e955e5b6aa0e3027b44795b2c2ebac932
jeonguihyeong <jwh4861230@likelion.org>
2021-08-03 10:35:54 +0900

    8 / 3 gis_p commit 5

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from articleapp.views import ArticleCreateView
+
 app_name = 'articleapp'
 
 #  accountapp:0000 에서 앞에accountapp같이 호출할때 app_name이 필요하다.
 
 urlpatterns = [
-    path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list')
+    path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
+
+    path('create/', ArticleCreateView.as_view(), name='create'),
+
+
 ]
