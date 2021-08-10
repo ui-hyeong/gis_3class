657cb527be42b3e75cf4a7505ab907c953b17a37
jeonguihyeong <jwh4861230@likelion.org>
2021-08-10 10:38:18 +0900

    8 / 10 gis_p commit 5

--- commentapp/views.py
+++ commentapp/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.views.generic import CreateView
 
+from articleapp.models import Article
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
 
@@ -12,6 +13,12 @@ class CommentCreateView(CreateView):
     model = Comment
     form_class = CommentCreationForm
     template_name = 'commentapp/create.html'
+
+    def form_valid(self, form):
+        form.instancewriter = self.request.user
+        # 작성자의 post의 article key를 article_id에 불러온다??
+        form.instance.article_id = self.request.POST.get('article_pk')
+        return super().form_valid(form)
 
     def get_success_url(self): # 댓글 작성후에 이동하는 주소
         return reverse('articleapp:detail', kwargs={'pk':self.object.article.pk})
