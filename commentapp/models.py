5e454ed847eb6a83d5603dd678e1a6ae12b5a79c
jeonguihyeong <jwh4861230@likelion.org>
2021-08-10 09:28:59 +0900

    8 / 10 gis_p commit 2

--- commentapp/models.py
+++ commentapp/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from articleapp.models import Article
+
+
+class Comment(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL,
+                               related_name='comment', null=True)
+    # 위는 작성자와 연결이다.
+    article = models.ForeignKey(Article, on_delete=models.SET_NULL,
+                                related_name='comment', null=True)
+
+    #textField는 장문의 글을 받을떄 씀
+    content = models.TextField(null=False)
+
+    #datetimefield는 시간과 날짜를 찍어줌
+    created_at = models.DateTimeField(auto_now_add=True)
