747846e0348ed3788926de73051d93d077687c0c
jeonguihyeong <jwh4861230@likelion.org>
2021-08-13 10:35:06 +0900

    8 / 13 gis_p commit 6

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,4 +1,12 @@
+from django.urls import path
+
+from projectapp.views import ProjectCreateView
+
+
+app_name = 'projectapp'
+# 이걸해야 projectapp:xxxxx 이 가능하다.
+
 
 urlpatterns = [
-
+    path('create/', ProjectCreateView.as_view(), name='create')
 ]
