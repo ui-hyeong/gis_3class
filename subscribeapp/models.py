1300959460ba8409138b7695ee48f66b8ce3dc65
jeonguihyeong <jwh4861230@likelion.org>
2021-08-20 14:49:04 +0900

    8 / 20 gis_p commit 1

--- subscribeapp/models.py
+++ subscribeapp/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='subscription', null=False)
+    # 구독자가 회원탈퇴 하면 casecade로인해 같이 삭제된다
+    project = models.ForeignKey(Project, on_delete=models.CASCADE,
+                                related_name='subscription', null=False)
+
+    class Meta:  #user와 project를 묶어준다.
+        unique_together = ['user', 'project']
