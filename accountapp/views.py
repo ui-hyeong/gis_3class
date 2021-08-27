ff15642edbfed42a1bffb47ed0c6bc825031a87c
jeonguihyeong <jwh4861230@likelion.org>
2021-08-27 09:15:07 +0900

    8 / 27 gis_p commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -16,31 +16,11 @@ from accountapp.models import newModel
 from articleapp.models import Article
 
 
-@login_required # login_url을 선언하여 경로를 설정해 줄 수 있다.
-def hello_world(request):
-
-    if request.method == "POST":
-
-        temp = request.POST.get('input_text')  # 인풋 텍스트를 얻어 temp라는 임시데이터에 저장한다.
-
-        model_instance: newModel = newModel()
-        model_instance.text = temp
-        model_instance.save()
-
-
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-      # accountapp의 hello_world의 이름으로 가라
-
-    else:
-        data_list = newModel.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
-
-
 
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/create.html' #어떤 html페이지를 쓸지
 
 class AccountDetailView(DetailView, MultipleObjectMixin):
@@ -72,6 +52,6 @@ class AccountUpdateView(UpdateView):
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/delete.html'
 
